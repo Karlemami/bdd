a9be5c9838e6ab31c919961826ebde32e6183e58
valentinefleith <valentine.fleith@gmail.com>
2023-12-01 14:18:07 +0100

    add method in class mail

--- class_Mail/main.py
+++ class_Mail/main.py
@@ -1,10 +1,17 @@
+import re 
+
 from Mail import Mail
 
 
 def main():
-    line = ("03/07/199", "Bonjour Au revoir", "Reunion", 4, 7)
+    line = ("03/07/199", "Bonjour PDG Au revoir PDG", "Reunion", 4, 7)
     mail = Mail(line)
-    print(mail.text)
+    #mots_importants = ["PDG", "Boss", "connard"]
+    #found = []
+    #for line in lines:
+        #if mail.print_when_expr_found("PDG"):
+            #found.append(line)
+    mail.print_when_expr_found(mots_importants)
 
 
 main()
