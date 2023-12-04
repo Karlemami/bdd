8c773c866f646c748991c67544a106f5d64c131b
valentinefleith <valentine.fleith@gmail.com>
2023-12-04 16:33:17 +0100

    update request in main

--- class_Mail/main.py
+++ class_Mail/main.py
@@ -1,11 +1,15 @@
-import re 
+import sqlite3
 
 from Mail import Mail
 
 
 def main():
-    #line = ("03/07/199", "Bonjour PDG Au revoir PDG", "Reunion", 4, 7)
-    for line in result:
+    conn = sqlite3.connect("../enquete.db")
+    c = conn.cursor()
+    mails = c.execute("SELECT * FROM mails")
+    mails = mails.fetchall()
+
+    for line in mails:
         mail = Mail(line)
         regex_boss = "(PDG|[Bb]oss|[Pp]atron|Jean Dubois)"
         regex_project = "((projet)?[tT]itan[aA][iI])"
