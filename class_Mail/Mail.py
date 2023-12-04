734c98b5c6cf3b134f8159b3db6afb78e594c7e5
valentinefleith <valentine.fleith@gmail.com>
2023-12-04 16:30:37 +0100

    update class mail and regex

--- class_Mail/Mail.py
+++ class_Mail/Mail.py
@@ -2,38 +2,22 @@ import re
 
 
 class Mail:
-    """
-    A class to represent a mail.
-
-    Attributes
-    ----------
-    subject : str
-    text : str
-    id_exp : int
-    id_dest : int
-    """
-
     def __init__(self, line: tuple):
         self.text = line[1]
         self.subject = line[2]
         self.id_exp = line[3]
         self.id_dest = line[4]
 
-    def find_expr(self, expr):
-        occ = re.findall(expr, self.text)
-        occ.append(re.findall(expr, self.subject))
-        return occ
-
-    def find_regex_in_mails(self, expr):
+    def find_regex_in_mails(self, expr, path="mails_trouves.txt"):
         if re.search(expr, self.text):
-            write_in_file(f"\nOccurrence trouvee dans le mail suivant :\n {self.text}")
-            write_in_file(f"\nL'objet du mail est : {self.subject}\n")
-            write_in_file(f"\nL'id de l'expediteur est : {self.id_exp}\n")
-            write_in_file(f"\nL'id du destinataire est : {self.id_dest}\n")
+            write_in_file(f"\nOccurrence trouvee dans le mail suivant :\n {self.text}", path)
+            write_in_file(f"\nL'objet du mail est : {self.subject}\n", path)
+            write_in_file(f"\nL'id de l'expediteur est : {self.id_exp}\n", path)
+            write_in_file(f"\nL'id du destinataire est : {self.id_dest}\n", path)
         return True
 
 
-def write_in_file(text, path="mails_trouves.txt"):
+def write_in_file(text, path):
     with open(path, "a") as file:
         file.write(text)
 
--- class_Mail/main.py
+++ class_Mail/main.py
@@ -4,10 +4,13 @@ from Mail import Mail
 
 
 def main():
-    line = ("03/07/199", "Bonjour PDG Au revoir PDG", "Reunion", 4, 7)
-    mail = Mail(line)
-    regex = "(PDG|[Bb]oss|[Pp]atron|Jean Dubois)"
-    mail.find_regex_in_mails(regex)
+    #line = ("03/07/199", "Bonjour PDG Au revoir PDG", "Reunion", 4, 7)
+    for line in result:
+        mail = Mail(line)
+        regex_boss = "(PDG|[Bb]oss|[Pp]atron|Jean Dubois)"
+        regex_project = "((projet)?[tT]itan[aA][iI])"
+        mail.find_regex_in_mails(regex_boss, "mails_boss.txt")
+        mail.find_regex_in_mails(regex_project, "mails_project.txt")
 
 
 main()
